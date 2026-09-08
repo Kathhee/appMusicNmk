0e7dce186b5abd537a5fb05329fcd3aa50753213
kathhee <francakathellin@gmail.com>
2026-09-08 14:06:11 -0300

    hosting

---
+++ js/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCfySKrAxwQCUF2dcdk3gVZbU8t8xCRbcw",
+  authDomain: "nmkmusicproject.firebaseapp.com",
+  projectId: "nmkmusicproject",
+  storageBucket: "nmkmusicproject.firebasestorage.app",
+  messagingSenderId: "178805272376",
+  appId: "1:178805272376:web:f2908e5bcf18e79c2f1bcd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
